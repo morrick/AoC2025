4ed173bd39777d21b7203375d8ca52c5964f2351
Miles Orrick <miles.orrick@gmail.com>
2025-12-04 12:16:21 -0500

    finished day 4 puzz 2

--- AoC4-2.py
+++ AoC4-2.py
@@ -1,8 +1,74 @@
-def check(x, y):
-    if x >= 0 and x < w and y >= 0 and y < h and rolls[x][y] == "@":
+def print_map(map):
+    for row in map:
+        print(row)
+
+
+def check(map, x, y):
+    if x >= 0 and x < w and y >= 0 and y < h and map[x][y] == "@":
         return True
     else:
         return False
+
+
+def run_map(map):
+    global total
+    counter = 0
+    newmap = ""
+
+    for x in range(w):
+        for y in range(h):
+            if map[x][y] == ".":
+                newmap += "."
+                continue
+
+            neighbours = 0
+            # check N
+            if check(map, x, y - 1):
+                neighbours += 1
+            # check NE
+            if check(map, x + 1, y - 1):
+                neighbours += 1
+            # check E
+            if check(map, x + 1, y):
+                neighbours += 1
+            # check SE
+            if check(map, x + 1, y + 1):
+                neighbours += 1
+            # check S
+            if check(map, x, y + 1):
+                neighbours += 1
+            # check SW
+            if check(map, x - 1, y + 1):
+                neighbours += 1
+            # check W
+            if check(map, x - 1, y):
+                neighbours += 1
+            # check NW
+            if check(map, x - 1, y - 1):
+                neighbours += 1
+
+            if neighbours < 4:
+                newmap += "x"
+                counter += 1
+            else:
+                newmap += "@"
+
+    if counter == 0:
+        return
+    else:
+        total += counter
+        tempmap = []
+        for i in range(h):
+            temp_char = ""
+            for j in range(w):
+                temp_char += (
+                    "."
+                    if newmap[i * w + j] == "." or newmap[i * w + j] == "x"
+                    else newmap[i * w + j]
+                )
+            tempmap.append(temp_char)
+
+        run_map(tempmap)
 
 
 # file = open("AoC4-1-test.txt")
@@ -11,70 +77,12 @@ rolls = file.read().splitlines()
 
 h = len(rolls)
 w = len(rolls[0])
-newmap = ""
-counter = 0
 
-for x in range(w):
-    for y in range(h):
-        if rolls[x][y] == ".":
-            # print(x, y, "position is empty marking .")
-            newmap += "."
-            continue
+total = 0
 
-        neighbours = 0
-        # check N
-        if check(x, y - 1):
-            neighbours += 1
-        # check NE
-        if check(x + 1, y - 1):
-            neighbours += 1
-        # check E
-        if check(x + 1, y):
-            neighbours += 1
-        # check SE
-        if check(x + 1, y + 1):
-            neighbours += 1
-        # check S
-        if check(x, y + 1):
-            neighbours += 1
-        # check SW
-        if check(x - 1, y + 1):
-            neighbours += 1
-        # check W
-        if check(x - 1, y):
-            neighbours += 1
-        # check NW
-        if check(x - 1, y - 1):
-            neighbours += 1
+run_map(rolls)
 
-        if neighbours < 4:
-            # print(x, y, "neighbours = ", neighbours, " marking x")
-            newmap += "x"
-            counter += 1
-        else:
-            # print(x, y, "neighbours = ", neighbours, " marking @")
-            newmap += "@"
+print(total)
 
-# for i in range(h):
-#     for j in range(w):
-#         print(newmap[i * w + j], end="")
-#     print("")
-
-print(counter)
-
-# The forklifts can only access a roll of paper if there are fewer than four rolls of paper in the eight adjacent positions.
-
-# part 1 test answer:
-# there are 13 x's
-"""
-..xx.xx@x.
-x@@.@.@.@@
-@@@@@.x.@@
-@.@@@@..@.
-x@.@@@@.@x
-.@@@@@@@.@
-.@.@.@.@@@
-x.@@@.@@@@
-.@@@@@@@@.
-x.x.@@@.x.
-"""
+# part 2 test answer:
+# there are 43 x's removed
